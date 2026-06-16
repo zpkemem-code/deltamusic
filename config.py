91ac007f243ba8ff3104cc61228d5b339fad2d0d
zpcode <zpkemem@gmail.com>
2026-06-16 15:20:56 +0800

    Change default DB_NAME from 'DeltaMusic' to 'StreamingMusic'

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 
         self.BOT_TOKEN = getenv("BOT_TOKEN")
         self.MONGO_URL = getenv("MONGO_URL")
-        self.DB_NAME = getenv("DB_NAME", "DeltaMusic")
+        self.DB_NAME = getenv("DB_NAME", "StreamingMusic")
 
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
