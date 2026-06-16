d13a5bbffbe32eabb2c2a0dc5a452b2a0ab9e8a7
zpcode <zpkemem@gmail.com>
2026-06-16 23:46:12 +0800

    Remove pause and resume buttons from controls
    Removed pause and resume controls from keyboard.

--- delta/helpers/_inline.py
+++ delta/helpers/_inline.py
@@ -38,8 +38,6 @@ class Inline:
             keyboard.append(
                 [
                     self.ikb(text="⏹", callback_data=f"controls stop {chat_id}"),
-                    self.ikb(text="⏸", callback_data=f"controls pause {chat_id}"),
-                    self.ikb(text="▶️", callback_data=f"controls resume {chat_id}"),
                     self.ikb(text="⏭", callback_data=f"controls skip {chat_id}"),
                 ]
             )
